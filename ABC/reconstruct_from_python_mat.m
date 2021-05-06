3729eec83d8baea799dfe5f0e7f29ed445ae1531
Indigo Mad <44008690+IndigoMad@users.noreply.github.com>
2021-05-06 14:39:03 +0800

    Update reconstruct_from_python_mat.m

--- ABC/reconstruct_from_python_mat.m
+++ ABC/reconstruct_from_python_mat.m
@@ -1,8 +1,11 @@
+%load mat file and save as json file
 load('ABC.mat')
 txt=jsonencode(ABC);
 f=fopen('ABC.json','w','n','UTF-8');
 fprintf(f,txt);
 fclose(f);
+
+%update mat file from the json file
 f=fopen('ABC.json','r','n','UTF-8');
 txt=fgets(f);
 fclose(f);
