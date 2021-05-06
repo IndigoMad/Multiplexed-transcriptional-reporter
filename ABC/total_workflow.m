b7461dcdfe0cd25b2896b3742f510284d90e8a64
Indigo Mad <44008690+IndigoMad@users.noreply.github.com>
2021-05-06 16:55:21 +0800

    Add files via upload

---
+++ ABC/total_workflow.m
@@ -0,0 +1,21 @@
+%Reshape the data in oder to store in json files.
+reconstruct_from_python_mat
+
+%Extract burst information including starting, ending and duration of the
+%bursts
+bursts_information
+
+%Regulation relationship between upstream and downstream including the upstream intensity and response time. 
+regulation_relationship
+
+%Partition of gene B into two group. 
+Gene_B_partition
+
+%cross-correlation, correlation coefficcient, etc.
+correlation
+
+%Estimate parameters for the model 
+parameter_fitting
+
+%write the processed data into a json file
+write_json
