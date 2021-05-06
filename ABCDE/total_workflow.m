8a0ba04e28fb511b215d0e3fd180819d9f9b2019
Indigo Mad <44008690+IndigoMad@users.noreply.github.com>
2021-05-06 19:50:35 +0800

    Add files via upload

---
+++ ABCDE/total_workflow.m
@@ -0,0 +1,12 @@
+%Reshape the data in oder to store in json files.
+reconstruct_from_python_mat
+
+%Extract burst information including starting, ending and duration of the
+%bursts
+bursts_information
+
+%Regulation relationship between upstream and downstream including the upstream intensity and response time. 
+regulation_relationship
+
+%write the processed data into a json file
+write_json
